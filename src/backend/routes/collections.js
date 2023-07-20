5cc3d2fb28c245bc9b7628441d9df52b1f6ea24f
Vegadelalyra <lyradeperseo@gmail.com>
2023-07-19 22:37:11 -0500

    Routing collection && testing importMap

--- src/backend/routes/collections.js
+++ src/backend/routes/collections.js
@@ -1,6 +1,26 @@
 import { server } from './pageTitle&dbURL.js'
 
+
+
 export async function collections () {
     console.log('[ /#collections ] rendered.')
-    // document.body.innerHTML = '<h1>hola</h1>'
+
+        // Get template
+        const templatePath = '/src/backend/templates/collections.html'
+        let template = await fetch(templatePath)
+        .then(html => html.text())
+        document.body.innerHTML = template
+
+        // [ CHANGE HTML ELEMENTS ]
+        const getItem = bem => document.querySelector(bem)
+
+        // litle fixes to styles
+        document.body.style.overflow = 'auto'
+
+        // title
+        document.title = server.PageTitle + ' | ' + 'Collections'
+
+        // metadata
+        getItem('meta[name="description"]')
+        .setAttribute('content', 'SNEAKERS Autumn Limited Collection')
 }
