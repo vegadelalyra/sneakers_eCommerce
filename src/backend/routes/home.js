3dca53827507e30f3cfb49f94e7e840a6bf1bda1
Vegadelalyra <lyradeperseo@gmail.com>
2023-07-19 21:44:13 -0500

    Preloader added

--- src/backend/routes/home.js
+++ src/backend/routes/home.js
@@ -10,23 +10,10 @@ export async function home () {
         return sessionStorage.removeItem('#') }
     sessionStorage.setItem('#', 1)
 
-    // Get template
-    const templatePath = '/src/backend/templates/home.html'
-    let template = await fetch(templatePath)
-    .then(html => html.text())
-    document.body.innerHTML = template
-
     // Get random product [just for fun]
-    const endPoint = server.endPoint
-
-    const {indexes, products: Products} = await fetch(endPoint)
+    const indexes = await fetch(server.endPoint)
         .then(res => res.json())
-        .then(json => {
-            return { 
-                products: json.products,
-                indexes : json.products.map(p => p.id)
-            }
-        })
+        .then(json => json.products.map(p => p.id))
 
     // Guarantee to never fetch the same product
     let any, otherProduct = () => {
@@ -36,45 +23,7 @@ export async function home () {
         : sessionStorage.setItem('productID', any)
     }; otherProduct()
 
-    // fetch a random different product
-    const randomProduct = Products[any]
+    // reroute to a single page of a random product
     localStorage.setItem('fetched product', JSON.stringify(any))
     return window.location.hash = '#product/' + any
-}
-
-async function renderHome() {
-    // [Change html elements]
-    const getItem = bem => document.querySelector(bem)
-
-    // title
-    document.title = server.PageTitle + ' | ' + randomProduct.name 
-
-    // metadata
-    getItem('meta[name="description"]')
-    .setAttribute('content', randomProduct.name)
-
-    // price
-    getItem('.details__before').innerHTML = `$${randomProduct.price * 2}.00`    
-    getItem('.details__now').innerHTML = `
-        $${randomProduct.price}.00<span class="details__discount">50%</span>`
-
-    // info
-    getItem('.details__title').innerText = randomProduct.name
-    getItem('.details__description').innerText = randomProduct.about
-    
-    // pics 
-    document.querySelectorAll('.gallery__thumbnail')
-    .forEach((thumbnail, index) => thumbnail.src = randomProduct.imgs[index])        
-    document.querySelectorAll('.modal-gallery__thumbnail') 
-    .forEach((thumbnail, index) => thumbnail.src = randomProduct.imgs[index])        
-
-    // main img
-    const mainImg_curatedURL = randomProduct.imgs[0]
-    .replace('c_thumb,w_200,g_face', 'w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai') 
-    
-    document.querySelector('.gallery__image-container')
-    .style.backgroundImage = `url(${mainImg_curatedURL})`
-
-    // Import and attach all frontend events
-    await import('/src/frontend.js')
 }
--- src/backend/routes/product.js
+++ src/backend/routes/product.js
@@ -1,13 +1,58 @@
 import { server } from './pageTitle&dbURL.js'
 
-export function product () {
-    const productID = localStorage
-    .getItem('fetched product')
-    
-    // Notify about sucess
+export async function product () {
+    // Get product ID to render and notify about sucess
+    const productID = localStorage.getItem('fetched product')
     console.log('[ /', window.location.hash, '] rendered.')
 
-    document.body.innerHTML = '<h1>hola</h1>'
-    console.log('funcionó? xd', localStorage
-    .getItem('fetched product'))
+    // Fetch the randomnly picked product
+    const Product = await fetch(server.endPoint)
+    .then(res => res.json())
+    .then(json => json.products[productID])
+
+    // Get template
+    const templatePath = '/src/backend/templates/home.html'
+    let template = await fetch(templatePath)
+    .then(html => html.text())
+    document.body.innerHTML = template
+
+    // then, render it!
+    renderRandom(Product) 
+}
+
+async function renderRandom(randomProduct) {
+    // [Change html elements]
+    const getItem = bem => document.querySelector(bem)
+
+    // title
+    document.title = server.PageTitle + ' | ' + randomProduct.name 
+
+    // metadata
+    getItem('meta[name="description"]')
+    .setAttribute('content', randomProduct.name)
+
+    // price
+    getItem('.details__before').innerHTML = `$${randomProduct.price * 2}.00`    
+    getItem('.details__now').innerHTML = `
+        $${randomProduct.price}.00<span class="details__discount">50%</span>`
+
+    // info
+    getItem('.details__title').innerText = randomProduct.name
+    getItem('.details__description').innerText = randomProduct.about
+    
+    // pics 
+    document.querySelectorAll('.gallery__thumbnail')
+    .forEach((thumbnail, index) => thumbnail.src = randomProduct.imgs[index])        
+    document.querySelectorAll('.modal-gallery__thumbnail') 
+    .forEach((thumbnail, index) => thumbnail.src = randomProduct.imgs[index])        
+
+    // main img
+    const mainImg_curatedURL = randomProduct.imgs[0]
+    .replace('c_thumb,w_200,g_face', 'w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai') 
+    
+    document.querySelector('.gallery__image-container')
+    .style.backgroundImage = `url(${mainImg_curatedURL})`
+
+    // Import and attach all frontend events
+    await import('/src/frontend.js')
 }
